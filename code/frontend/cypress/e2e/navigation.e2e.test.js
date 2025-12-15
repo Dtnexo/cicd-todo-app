36b13369c27a9b41f5c1cfc2a762270dfc624f5d
Evinne8 <pe41bnd@eduvaud.ch>
2025-12-15 11:07:11 +0100

    test(navigationE2E): Faire le test de redirection si non connécté sur cypress sur la navigation
    [15MIN][DONE]
    
    Si l'utilisateur n'est pas connécté, alors il est redirigé sur la page de login.

---
+++ code/frontend/cypress/e2e/navigation.e2e.test.js
@@ -0,0 +1,19 @@
+describe('Navigation', () => {
+  beforeEach(() => {
+    cy.task('clearDB');
+    cy.clearAllLocalStorage();
+    cy.clearAllCookies();
+    cy.visit('/');
+  });
+
+  it('je peux naviguer entre les pages publiques', () => {
+    // Je clique sur le lien vers l’accueil (logo ou lien principal)
+    cy.get('a[href="/"]').first().click();
+
+    // Si je ne suis pas connecté, je suis redirigé vers la page de login
+    cy.url().should('eq', Cypress.config().baseUrl + '/login');
+  });
+
+    cy.get('.profile-menu').should('exist');
+  });
+});
